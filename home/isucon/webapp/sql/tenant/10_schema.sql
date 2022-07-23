f635445c40a3728ee2d558449e876c0ee8d86233
シン・ウー馬場ーイー2 <shin-ubabar-eat2@isucon.net>
2022-07-23 03:53:57 +0000

    sqliteインデックス追加 6785

--- home/isucon/webapp/sql/tenant/10_schema.sql
+++ home/isucon/webapp/sql/tenant/10_schema.sql
@@ -30,3 +30,8 @@ CREATE TABLE player_score (
   created_at BIGINT NOT NULL,
   updated_at BIGINT NOT NULL
 );
+
+CREATE INDEX player_score_idx1 ON player_score (tenant_id, competition_id, player_id, row_num DESC);
+CREATE INDEX player_score_idx2 ON player_score (tenant_id, competition_id, row_num DESC);
+CREATE INDEX competition_idx1 ON competition (tenant_id, created_at DESC);
+CREATE INDEX player_idx1 ON player (tenant_id, created_at);
